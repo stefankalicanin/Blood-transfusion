4964b40b495d12acb2f11b2a046105467a61682b
s-matke <stefan.matkovic@hotmail.com>
2022-10-31 17:26:45 +0100

    init commit

---
+++ src/main/java/java/ftn/uns/btb/BtbApplication.java
@@ -0,0 +1,13 @@
+package java.ftn.uns.btb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/java/ftn/uns/btb/BtbApplicationTests.java
@@ -0,0 +1,13 @@
+package java.ftn.uns.btb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
