a36bdf9200acda8add1b6c22ed4da5cb85b8cdf1
Stefan <skstefankalicanin@gmail.com>
2022-11-10 16:08:08 +0100

    Create admin and center

--- src/main/java/rs/ftn/uns/btb/service/AdminService.java
+++ src/main/java/rs/ftn/uns/btb/service/AdminService.java
@@ -1,5 +1,7 @@
 package rs.ftn.uns.btb.service;
 import rs.ftn.uns.btb.model.Admin;
+import rs.ftn.uns.btb.model.Staff;
 
 public interface AdminService {
+    Admin create(Admin admin) throws Exception;
 }
