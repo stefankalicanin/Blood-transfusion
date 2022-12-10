9a15e22f24ba7613e1c304ab89fa7618f81cd8e8
s-matke <stefan.matkovic@hotmail.com>
2022-12-10 19:34:36 +0100

    fetch roles; staff create refactor

--- BTB/src/main/java/rs/ftn/uns/btb/core/role/RoleRepository.java
+++ BTB/src/main/java/rs/ftn/uns/btb/core/role/RoleRepository.java
@@ -3,6 +3,8 @@ package rs.ftn.uns.btb.core.role;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
@@ -10,4 +12,6 @@ public interface RoleRepository extends JpaRepository<Role, Long> {
 
     Role findOneById(Long id);
     
+    @Query("select r from Role r where name in :names")
+    List<Role> findAllByName(@Param("names") List<String> roleNames);
 }
--- BTB/src/main/java/rs/ftn/uns/btb/core/staff/StaffServiceImpl.java
+++ BTB/src/main/java/rs/ftn/uns/btb/core/staff/StaffServiceImpl.java
@@ -1,12 +1,16 @@
 package rs.ftn.uns.btb.core.staff;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import rs.ftn.uns.btb.core.role.Role;
+import rs.ftn.uns.btb.core.role.RoleRepository;
+import rs.ftn.uns.btb.core.role.interfaces.IRole;
 import rs.ftn.uns.btb.core.staff.interfaces.StaffService;
 import rs.ftn.uns.btb.core.user.Roles;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -15,12 +19,32 @@ public class StaffServiceImpl implements StaffService {
     public final StaffRepository _staffRepo;
 
     @Autowired
+    private IRole _roleService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
     public StaffServiceImpl(StaffRepository _staffRepo) {
         this._staffRepo = _staffRepo;
     }
+
+    @Autowired
+    private RoleRepository _repo;
+
     @Override
     public Staff create(Staff staff) throws Exception {
-        staff.setRole(Roles.STAFF);
+        // staff.setRole(Roles.STAFF);
+        staff.setPassword(passwordEncoder.encode(staff.getPassword()));
+
+        // List<Role> roles = _roleService.findByName("ROLE_USER");
+
+        String[] strRoles = {"ROLE_USER", "ROLE_STAFF" };
+
+        List<Role> roles = _repo.findAllByName(Arrays.asList(strRoles));
+
+        staff.setRoles(roles);
+
         Staff newStaff = this._staffRepo.save(staff);
         return newStaff;
     }
